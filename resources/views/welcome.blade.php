5932803809731de0ab0d0d2062c4d43f21e3f061
Kristopher Villafuerte <villafuertekristophe33@gmail.com>
2026-05-24 23:37:38 +0800

    "minor edits"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
 
                     <div class="flex items-center justify-end mt-4">
                         @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                            <a class="text-white underline text-sm text-gray-600 hover:text-gray-900"
                             href="{{ route('password.request') }}">
                                 Forgot your password?
                             </a>
